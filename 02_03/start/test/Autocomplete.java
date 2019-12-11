d7fe6f43662b07ec1e422cf3c8aa4b2c4593479f
Shanaka Fernando <shanaka.fernando2@gmail.com>
2019-12-11 10:44:07 +0800

    Initial commit

---
+++ 02_03/start/test/Autocomplete.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Autocomplete {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/shanaka/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/autocomplete");
+
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+        autocomplete.sendKeys("1555 Park Blvd, Palo Alto, CA");
+        Thread.sleep(1000);
+
+        WebElement autocomleteResult = driver.findElement(By.className("pac-item"));
+        autocomleteResult.click();
+
+        driver.quit();
+    }
+}
---
+++ 04_02/start/test/Radiobuttons.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Checkboxes {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/shanaka/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/ckeckbox");
+
+        WebElement checkbox1 = driver.findElement(By.id("checkbox-1"));
+        checkbox1.click();
+
+        WebElement checkbox2 = driver.findElement(By.cssSelector("input[value='checkbox-2']"));
+        checkbox2.click();
+
+        WebElement checkbox3 = driver.findElement(By.xpath("//*[@id=\"checkbox-3\"]"));
+        checkbox3.click();
+
+        driver.quit();
+    }
+}
